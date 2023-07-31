6b1fc313e9b2fb3bf2017bd0a8adf385c4418f7d
Victor Hugo <111002821+victorhugopb@users.noreply.github.com>
2023-07-31 18:00:28 -0300

    Update aula70.js

--- aula70.js
+++ aula70.js
@@ -4,6 +4,7 @@ const teclaRes=document.querySelector(".res")
 const display=document.querySelector(".display")
 const ton=document.getElementById("ton")
 const tlimpar=document.getElementById("tlimpar")
+const tigual=document.getElementById("tigual")
 
 let sinal=false
 let decimal=false
@@ -11,13 +12,21 @@ let decimal=false
 teclasNum.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
         sinal=false
-        if(display.innerHTML=="0"){
+        if(evt.target.innerHTML==","){
+            if(!decimal){
+                decimal=true
+                if(display.innerHTML=="0"){
+                    display.innerHTML="0,"
+                }else{
+                display.innerHTML+=evt.target.innerHTML
+                }
+            }
+        }else{
+            if(display.innerHTML=="0"){
                 display.innerHTML=""
         }
-        if(evt.target.innerHTML==","){
-            decimal=true
+            display.innerHTML+=evt.target.innerHTML
         }
-        display.innerHTML+=evt.target.innerHTML
     })
 })
 
@@ -39,5 +48,13 @@ teclasOp.forEach((el)=>{
 
 tlimpar.addEventListener("click",(evt)=>{
     sinal=false
+    decimal=false
     display.innerHTML="0"
+})
+
+tigual.addEventListener("click",(evt)=>{
+    sinal=false
+    decimal=false
+    const res=eval(display.innerHTML)
+    display.innerHTML=res
 })
